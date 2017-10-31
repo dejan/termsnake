958dcac1a1938790385b02ec574d3081237eba6a
Dejan Simic <desimic@gmail.com>
2017-10-31 23:08:47 +0100

    Better controls and drawing

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 	"github.com/pkg/term"
 )
 
-type direction uint8
+type direction int8
 
 const (
-	up    = direction(5)
-	down  = direction(6)
-	right = direction(7)
-	left  = direction(8)
+	up    = direction(-1)
+	down  = direction(1)
+	right = direction(2)
+	left  = direction(-2)
 )
 
 type node struct {
@@ -23,55 +23,60 @@ type node struct {
 	d direction
 }
 
-func (n *node) move() {
-	switch n.d {
-	case up:
-		n.y--
-	case down:
-		n.y++
-	case left:
-		n.x--
-	case right:
-		n.x++
-	}
-}
-
 type snake struct {
 	nodes []*node
+	d     direction
+}
+
+func (s *snake) head() *node {
+	return s.nodes[len(s.nodes)-1]
 }
 
 func (s *snake) move() {
-	for _, n := range s.nodes {
-		n.move()
+	s.head().d = s.d
+	for i, n := range s.nodes {
+		switch n.d {
+		case up:
+			n.y--
+		case down:
+			n.y++
+		case left:
+			n.x--
+		case right:
+			n.x++
+		}
+		if i < (len(s.nodes) - 1) {
+			n.d = s.nodes[i+1].d
+		}
 	}
 }
 
 func (s *snake) redirect(d direction) {
-	s.nodes[0].d = d
+	if (s.head().d + d) != 0 {
+		s.d = d
+	}
 }
 
 func (s *snake) draw() {
+	fmt.Printf("\033[2J")
 	for _, n := range s.nodes {
-		fmt.Printf("\033[2J\033[%d;%dHX", n.y, n.x)
+		fmt.Printf("\033[%d;%dHX", n.y, n.x)
 	}
-}
-
-func getch() []byte {
-	t, _ := term.Open("/dev/tty")
-	term.RawMode(t)
-	bytes := make([]byte, 3)
-	numRead, err := t.Read(bytes)
-	t.Restore()
-	t.Close()
-	if err != nil {
-		return nil
-	}
-	return bytes[0:numRead]
 }
 
 func main() {
 	snake := snake{
-		nodes: []*node{&node{5, 5, right}},
+		nodes: []*node{
+			&node{2, 5, right},
+			&node{3, 5, right},
+			&node{4, 5, right},
+			&node{5, 5, right},
+			&node{6, 5, right},
+			&node{7, 5, right},
+			&node{8, 5, right},
+			&node{9, 5, right},
+		},
+		d: right,
 	}
 
 	ticker := time.NewTicker(80 * time.Millisecond)
@@ -80,10 +85,12 @@ func main() {
 
 	go keybordListener(redirections, exit)
 
+	fmt.Print("\033[?25l") // hide cursor
 	for {
 		select {
 		case <-exit:
-			fmt.Println()
+			fmt.Println("\033[?25h") // show cursor
+			fmt.Println("\033[0m")   // reset
 			return
 		case d := <-redirections:
 			snake.redirect(d)
@@ -91,7 +98,6 @@ func main() {
 			snake.move()
 			snake.draw()
 		default:
-
 		}
 	}
 }
@@ -114,4 +120,17 @@ func keybordListener(redirections chan direction, exit chan bool) {
 			fmt.Println("\nUnknown pressed", c)
 		}
 	}
+}
+
+func getch() []byte {
+	t, _ := term.Open("/dev/tty")
+	term.RawMode(t)
+	bytes := make([]byte, 3)
+	numRead, err := t.Read(bytes)
+	t.Restore()
+	t.Close()
+	if err != nil {
+		return nil
+	}
+	return bytes[0:numRead]
 }
